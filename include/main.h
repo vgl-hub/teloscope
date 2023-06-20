91c74d1d3fee02f65633f411aebbad1e322a8092
MacGooder <136636857+MacGooder@users.noreply.github.com>
2023-06-20 11:55:32 -0400

    Add files via upload

---
+++ include/main.h
@@ -0,0 +1,48 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string>
+#include <iostream>
+#include <unordered_map>
+
+#include <getopt.h>
+
+#include "log.h"
+
+#include "uid-generator.h"
+
+#include "bed.h"
+
+#include "global.h" // global variables
+#include "struct.h"
+#include "functions.h" // global functions
+#include <istream>
+#include <fstream>
+#include <sstream>
+
+#include <parallel_hashmap/phmap.h>
+
+#include "log.h"
+#include "global.h"
+#include "uid-generator.h"
+
+#include "bed.h"
+#include "struct.h"
+#include "functions.h"
+
+#include "gfa-lines.h"
+#include "gfa.h"
+#include "sak.h"
+
+#include "stream-obj.h"
+
+#include "input-agp.h"
+#include "input-filters.h"
+#include "input-gfa.h"
+
+#include "input.h"
+
+#endif /* MAIN_H */
