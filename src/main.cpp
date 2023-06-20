1327a594321d01edcd70b8040c6e0908c17bde07
MacGooder <136636857+MacGooder@users.noreply.github.com>
2023-06-20 12:02:03 -0400

    Add files via upload

---
+++ src/main.cpp
@@ -0,0 +1,147 @@
+#include <main.h>
+
+std::string version = "0.0.1";
+
+std::chrono::high_resolution_clock::time_point start = std::chrono::high_resolution_clock::now(); // immediately start the clock when the program is run
+short int tabular_flag;
+int verbose_flag;
+int seqReport_flag;
+int outSequence_flag;
+int nstarReport_flag;
+int outSize_flag;
+int outCoord_flag;
+int outFile_flag;
+int outBubbles_flag;
+int stats_flag;
+int rmGaps_flag;
+int discoverPaths_flag;
+int extractContigs_flag;
+int hc_flag;
+int hc_cutoff;
+int terminalOvlLen = 0;
+int maxThreads = 0;
+int cmd_flag;
+std::mutex mtx;
+ThreadPool<std::function<bool()>> threadPool;
+Log lg;
+
+int main(int argc, char **argv) {
+    
+    short int c; // optarg
+    short unsigned int pos_op = 1; // optional arguments
+    
+    bool arguments = true;
+    
+    std::string cmd;
+
+    bool isPipe = false; // to check if input is from pipe
+
+    UserInput userInput;
+    
+    if (argc == 1) { // gfastats with no arguments
+            
+        printf("teloscope [command]\n-h for additional help. Use -f to initiate the tool.\n");
+        exit(0);
+        
+    }
+    
+    static struct option long_options[] = { // struct mapping long options
+        {"input-sequence", required_argument, 0, 'f'},
+        {"verbose", no_argument, &verbose_flag, 1},
+        {"cmd", no_argument, &cmd_flag, 1},
+        {"version", no_argument, 0, 'v'},
+        {"help", no_argument, 0, 'h'},
+        
+        {0, 0, 0, 0}
+    };
+    
+    while (arguments) { // loop through argv
+        
+        int option_index = 0;
+        
+        c = getopt_long(argc, argv, "-:f:vh",
+                        long_options, &option_index);
+        
+        if (c == -1) { // exit the loop if run out of options
+            break;
+            
+        }
+
+        switch (c) {
+            case ':': // handle options without arguments
+                switch (optopt) { // the command line option last matched
+                    case 'b':
+                        break;
+                        
+                    default:
+                        fprintf(stderr, "option -%c is missing a required argument\n", optopt);
+                        return EXIT_FAILURE;
+                }
+                break;
+            default: // handle positional arguments
+                                
+            case 0: // case for long options without short options
+                
+//                if (strcmp(long_options[option_index].name,"line-length") == 0)
+//                  splitLength = atoi(optarg);
+                
+                break;
+
+            case 'f': // input sequence
+                
+                if (isPipe && userInput.pipeType == 'n') { // check whether input is from pipe and that pipe input was not already set
+                
+                    userInput.pipeType = 'f'; // pipe input is a sequence
+                
+                }else{ // input is a regular file
+                    
+                    ifFileExists(optarg);
+                    userInput.inSequence = optarg;
+                    
+                }
+                    
+                break;
+                
+            case 'v': // software version
+                printf("mytool v%s\n", version.c_str());
+                printf("Giulio Formenti giulio.formenti@gmail.com\n");
+                exit(0);
+                
+            case 'h': // help
+                printf("teloscope [command]\n");
+                printf("\nOptions:\n");
+                printf("-v --version software version.\n");
+                printf("--cmd print $0 to stdout.\n");
+                exit(0);
+        }
+        
+        if    (argc == 2 || // handle various cases in which the output should include summary stats
+              (argc == 3 && pos_op == 2) ||
+              (argc == 4 && pos_op == 3)) {
+            
+        }
+        
+    }
+    
+    if (cmd_flag) { // print command line
+        for (unsigned short int arg_counter = 0; arg_counter < argc; arg_counter++) {
+            printf("%s ", argv[arg_counter]);
+        }
+        printf("\n");
+        
+    }
+
+    threadPool.init(maxThreads); // initialize threadpool
+
+    Input in;
+    in.load(userInput); // load user input
+    lg.verbose("Loaded user input");
+    
+    InSequences inSequences; // initialize sequence collection object
+    lg.verbose("Sequence object generated");
+    in.read(inSequences); // read input content to inSequences container
+
+    exit(EXIT_SUCCESS);
+    
+}
+
