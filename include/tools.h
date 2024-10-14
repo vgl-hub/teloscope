8d75a95e32e52efc198900d0e0038642d6fcbb57
[AldhairMedico] <amedico@rockefeller.edu>
2024-10-14 12:20:36 -0400

    Oct 14, 2024: -p IUPAC and -c working

---
+++ include/tools.h
@@ -0,0 +1,9 @@
+#ifndef TOOLS_H
+#define TOOLS_H
+
+#include <vector>
+#include <string>
+
+void generate_combinations(const std::string &pattern, std::string &current, size_t index, std::vector<std::string> &combinations);
+
+#endif // TOOLS_H
